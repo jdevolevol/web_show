3b2442d8d4e54c327135e5ec14507e371bc20251
jdevolevol <jdevolevol@gmail.com>
2026-05-12 20:51:51 +0700

    change model AI

--- api/chat.js
+++ api/chat.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
           Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`
         },
         body: JSON.stringify({
-          model: "qwen/qwen3-32b:free",
+          model: "qwen/qwen3-coder:free",
           messages: [
             {
               role: "system",
